f53fe59e5968a1c31b6912fea6f674be6da0c76c
neufeldm <neufeldm@a9368e9a-fdf9-0310-b5ed-f0a418fb02d6>
2005-04-13 21:29:45 +0000

    Added outlines for some of the softmac functions
    git-svn-id: https://esl.cs.colorado.edu/svn/softmac@5 a9368e9a-fdf9-0310-b5ed-f0a418fb02d6

--- trunk/cheesymac/softmac_cheesymac.c
+++ trunk/cheesymac/softmac_cheesymac.c
@@ -1,15 +1,19 @@
 /*
- *  hello-5.c - Demonstrates command line argument passing to a module.
+ * softmac_cheesymac.c
+ * A "null" sample MAC layer for the CU SoftMAC toolkit
+ * This just runs a very simple "ethernet over wireless" protocol 
  */
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/stat.h>
+#include "cu_softmac_api.h"
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Peter Jay Salzman");
+MODULE_AUTHOR("Michael Neufeld");
 
+#if 0
 static short int myshort = 1;
 static int myint = 420;
 static long int mylong = 9999;
@@ -31,21 +35,72 @@ module_param(mylong, long, S_IRUSR);
 MODULE_PARM_DESC(mylong, "A long integer");
 module_param(mystring, charp, 0000);
 MODULE_PARM_DESC(mystring, "A character string");
+#endif
 
-static int __init hello_5_init(void)
+static int __init softmac_cheesymac_init(void)
 {
+#if 0
 	printk(KERN_ALERT "Hello, world 5\n=============\n");
 	printk(KERN_ALERT "myshort is a short integer: %hd\n", myshort);
 	printk(KERN_ALERT "myint is an integer: %d\n", myint);
 	printk(KERN_ALERT "mylong is a long integer: %ld\n", mylong);
 	printk(KERN_ALERT "mystring is a string: %s\n", mystring);
+#endif
 	return 0;
 }
 
-static void __exit hello_5_exit(void)
+static void __exit softmac_cheesymac_exit(void)
 {
 	printk(KERN_ALERT "Goodbye, world 5\n");
 }
 
-module_init(hello_5_init);
-module_exit(hello_5_exit);
+static int cu_softmac_packet_tx_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
+					  void* mydata,
+					  struct sk_buff* packet, int intop) {
+  int runagain = 0;
+
+  return runagain;
+}
+
+
+static int cu_softmac_packet_tx_done_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
+						void* mydata,
+						struct sk_buff* packet,
+						int intop) {
+  int runagain = 0;
+
+  return runagain;
+}
+
+
+static int cu_softmac_packet_rx_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
+					  void* mydata,
+					  struct sk_buff* packet,
+					  int intop) {
+  int runagain = 0;
+
+  return runagain;
+}
+
+static int cu_softmac_work_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
+				     void* mydata, int intop) {
+  int runagain = 0;
+
+  return runagain;
+}
+
+static int cu_softmac_detach_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
+				       void* mydata, int intop) {
+
+  return 0;
+}
+
+
+static int cu_softmac_create_instance_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh) {
+}
+
+static int cu_softmac_set_netifhandle_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh) {
+}
+
+module_init(softmac_cheesymac_init);
+module_exit(softmac_cheesymac_exit);
