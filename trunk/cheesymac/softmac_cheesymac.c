3508193206a399d5f009dc6b0b7e0ea2f3a84565
neufeldm <neufeldm@a9368e9a-fdf9-0310-b5ed-f0a418fb02d6>
2005-04-15 22:42:49 +0000

    Continuing to flesh out cheesymac functionality
    git-svn-id: https://esl.cs.colorado.edu/svn/softmac@6 a9368e9a-fdf9-0310-b5ed-f0a418fb02d6

--- trunk/cheesymac/softmac_cheesymac.c
+++ trunk/cheesymac/softmac_cheesymac.c
@@ -13,6 +13,14 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michael Neufeld");
 
+typedef struct {
+  struct list_head list;
+  CU_SOFTMAC_NETIF_HANDLE mynfh;
+  unsigned char txbitrate;
+} CHEESYMAC_INSTANCE;
+
+static CHEESYMAC_INSTANCE* my_softmac_instances = 0;
+
 #if 0
 static short int myshort = 1;
 static int myint = 420;
@@ -51,13 +59,30 @@ static int __init softmac_cheesymac_init(void)
 
 static void __exit softmac_cheesymac_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, world 5\n");
+	printk(KERN_ALERT "Unloading CheesyMAC\n");
+	/*
+	 * Tell any/all softmac PHY layers that we're leaving
+	 */
+	if (my_softmac_instances) {
+	  CHEESYMAC_INSTANCE* cheesy_instance = 0;
+	  struct list_head* p = 0;
+	  
+	  list_for_each(p,my_softmac_instances->list) {
+	    cheesy_instance = list_entry(p,CHEESYMAC_INSTANCE,list);
+	    cu_softmac_detach_mac(cheesy_instance->mynfh,cheesy_instance);
+	  }
+	}
 }
 
 static int cu_softmac_packet_tx_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
 					  void* mydata,
 					  struct sk_buff* packet, int intop) {
+  CHEESYMAC_INSTANCE* myinstance = mydata;
   int runagain = 0;
+
+  if (myinstance) {
+    cu_softmac_sendpacket(nfh,256,packet);
+  }
 
   return runagain;
 }
@@ -68,6 +93,14 @@ static int cu_softmac_packet_tx_done_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
 						struct sk_buff* packet,
 						int intop) {
   int runagain = 0;
+  CHEESYMAC_INSTANCE* myinstance = mydata;
+
+  if (myinstance) {
+    if (packet) {
+      cu_softmac_free_skb(nfh,packet);
+      packet = 0;
+    }
+  }
 
   return runagain;
 }
@@ -77,30 +110,84 @@ static int cu_softmac_packet_rx_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
 					  void* mydata,
 					  struct sk_buff* packet,
 					  int intop) {
+  CHEESYMAC_INSTANCE* myinstance = mydata;
   int runagain = 0;
+
+  if (myinstance) {
+    cu_softmac_netif_rx_ether(nfh,packet);
+  }
 
   return runagain;
 }
 
 static int cu_softmac_work_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
 				     void* mydata, int intop) {
+  CHEESYMAC_INSTANCE* myinstance = mydata;
   int runagain = 0;
+
+  if (myinstance) {
+  }
 
   return runagain;
 }
 
 static int cu_softmac_detach_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,
 				       void* mydata, int intop) {
-
+  CHEESYMAC_INSTANCE* myinstance = mydata;
+  if (myinstance) {
+  }
   return 0;
 }
 
 
-static int cu_softmac_create_instance_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh) {
+static int cu_softmac_create_instance_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,CU_SOFTMAC_CLIENT_INFO* clientinfo) {
+  int result = 0;
+  CHEESYMAC_INSTANCE* newinst = 0;
+
+  newinst = kmalloc(sizeof(CHEESYMAC_INSTANCE),GFP_ATOMIC);
+  if (newinst) {
+    memset(newinst,0,sizeof(CHEESYMAC_INSTANCE));
+    INIT_LIST_HEAD(&newinst->list);
+
+    if (!my_softmac_instances) {
+      my_softmac_instances = newinst;
+    }
+    else {
+      list_add_tail(&newinst->list,&my_softmac_instances->list);
+    }
+    clientinfo->client_private = newinst;
+    newinst->mynfh = nfh;
+    newinst->txbitrate = 2;
+    set_cheesymac_functions(clientinfo);
+  }
+  else {
+    result = -1;
+  }
+
+  return result;
 }
 
-static int cu_softmac_set_netifhandle_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh) {
+static int cu_softmac_set_netifhandle_cheesymac(CU_SOFTMAC_NETIFHANDLE nfh,void* mypriv) {
+  int result = 0;
+  if (mypriv) {
+    CHEESYMAC_INSTANCE* myinst = mypriv;
+    myinst->mynfh= nfh;
+    result = 0;
+  }
+  else {
+    result = -1;
+  }
+  return result;
 }
 
+static void set_cheesymac_functions(CU_SOFTMAC_CLIENT_INFO* clientinfo) {
+  clientinfo->cu_softmac_packet_tx = cu_softmac_packet_tx_cheesymac;
+  clientinfo->cu_softmac_packet_tx_done = cu_softmac_packet_tx_done_cheesymac;
+  clientinfo->cu_softmac_packet_rx = cu_softmac_packet_rx_cheesymac;
+  clientinfo->cu_softmac_work = cu_softmac_work_cheesymac;
+  clientinfo->cu_softmac_detach = cu_softmac_detach_cheesymac;
+}
 module_init(softmac_cheesymac_init);
 module_exit(softmac_cheesymac_exit);
+
+EXPORT_SYMBOL(cu_softmac_create_instance_cheesymac);
