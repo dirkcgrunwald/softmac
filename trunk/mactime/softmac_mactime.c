f84bd0d3fffba76424414c5689d72f9ad039986b
neufeldm <neufeldm@a9368e9a-fdf9-0310-b5ed-f0a418fb02d6>
2005-05-05 14:43:27 +0000

    Fixed up compile errors, added some missing functionality
    git-svn-id: https://esl.cs.colorado.edu/svn/softmac@24 a9368e9a-fdf9-0310-b5ed-f0a418fb02d6

--- trunk/mactime/softmac_mactime.c
+++ trunk/mactime/softmac_mactime.c
@@ -9,6 +9,7 @@
 #include <linux/init.h>
 #include <linux/stat.h>
 #include "../cu_softmac_api.h"
+#include "softmac_mactime.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michael Neufeld");
@@ -18,15 +19,17 @@ MODULE_AUTHOR("Michael Neufeld");
  * the client MAC layer.
  */
 
-// >0 -> time until slot arrives
-// <=0 -> -(time left in slot)
-static int32_t
+/*
+ * >0 -> time until slot arrives
+ * <=0 -> -(time left in slot)
+ */
+int32_t
 cu_softmac_mactime_tdma_slotstatus(CU_SOFTMAC_MACTIME_STATE* mts,
 				   u_int32_t* pcurslot) {
   int32_t result = 0;
   if (mts && mts->phyinfo && mts->phyinfo->cu_softmac_get_time) {
     u_int32_t now = mts->phyinfo->cu_softmac_get_time(mts->phyinfo->phyhandle);
-    u_int32_t slotcount = (now/sc->mts->tdma_slotlen);
+    u_int32_t slotcount = (now/mts->tdma_slotlen);
     u_int32_t curslot = mts->tdma_slotcount;
     u_int64_t curslotstart = slotcount * mts->tdma_slotlen;
     u_int64_t curslotend = curslotstart + mts->tdma_slotlen;
@@ -55,7 +58,7 @@ cu_softmac_mactime_tdma_slotstatus(CU_SOFTMAC_MACTIME_STATE* mts,
   return result;
 }
 
-static int32_t
+int32_t
 cu_softmac_mactime_tdma_timetonextslot(CU_SOFTMAC_MACTIME_STATE* mts) {
   int32_t result = -1;
   if (mts && mts->phyinfo && mts->phyinfo->cu_softmac_get_time) {
@@ -86,16 +89,55 @@ cu_softmac_mactime_tdma_timetonextslot(CU_SOFTMAC_MACTIME_STATE* mts) {
  * time until we can transmit, <0 means that the packet is too big
  * to fit in the timeslot, i.e. we'll never be able to send it.
  */
-static int32_t
+int32_t
 cu_softmac_mactime_tdma_xmitwhen(CU_SOFTMAC_MACTIME_STATE* mts,
 				 struct sk_buff* packet) {
   int32_t result = 0;
   if (mts && mts->phyinfo &&
       mts->phyinfo->cu_softmac_get_duration &&
       mts->phyinfo->cu_softmac_get_txlatency) {
+    CU_SOFTMAC_PHY_HANDLE phyhandle = mts->phyinfo->phyhandle;
     int32_t slotstat = cu_softmac_mactime_tdma_slotstatus(mts,0);
-    int32_t txlat = mts->phyinfo->cu_softmac_get_txlatency();
-    int32_t pktlen = mts->phyinfo->cu_softmac_get_duration();
+    int32_t txlat = (mts->phyinfo->cu_softmac_get_txlatency)(phyhandle);
+    int32_t pktlen = (mts->phyinfo->cu_softmac_get_duration)(phyhandle,
+							     packet);
+    int32_t maxtxlen = mts->tdma_slotlen - 2*mts->tdma_guardtime;
+
+    if (pktlen > maxtxlen) {
+      /*
+       * Not feasible to EVER transmit packet in this slot length
+       */
+      result = maxtxlen - pktlen;
+    }
+    else if (0 > slotstat) {
+      /*
+       * We're in our slot. We can transmit now if we're at least
+       * (guard time - lead time) into the slot and
+       * there's at least (packet length + guard time + tx latency)
+       * time left in the slot, otherwise we have to wait for
+       * our next slot to begin.
+       */
+      int32_t timefromstart = mts->tdma_slotlen + slotstat;
+      int32_t timeleft = -slotstat;
+      if ( (timefromstart >= (mts->tdma_guardtime - txlat)) &&
+	   (timeleft >= (pktlen + mts->tdma_guardtime + txlat)) ) {
+	result = 0;
+      }
+      else {
+	/*
+	 * Not enough time left, figure out how long we have to
+	 * wait to catch the next slot.
+	 */
+	result = cu_softmac_mactime_tdma_timetonextslot(mts) +
+	  mts->tdma_guardtime - txlat;
+      }
+    }
+    else {
+      /*
+       * We aren't in our slot, wait until our next slot comes up
+       */
+      result = slotstat + mts->tdma_guardtime - txlat;
+    }
   }
   return result;
 }
--- trunk/mactime/softmac_mactime.h
+++ trunk/mactime/softmac_mactime.h
@@ -26,4 +26,16 @@ typedef struct {
 int32_t cu_softmac_mactime_tdma_slotstatus(CU_SOFTMAC_MACTIME_STATE* mts,
 					   u_int32_t* pcurslot);
 
+/*
+ * How long until the next transmission slot begins?
+ */
 int32_t cu_softmac_mactime_tdma_timetonextslot(CU_SOFTMAC_MACTIME_STATE* mts);
+
+/*
+ * How long until we can transmit the packet? Return value >=0 means
+ * time until we can transmit, <0 means that the packet is too big
+ * to fit in the timeslot, i.e. we'll never be able to send it.
+ */
+int32_t
+cu_softmac_mactime_tdma_xmitwhen(CU_SOFTMAC_MACTIME_STATE* mts,
+				 struct sk_buff* packet);
