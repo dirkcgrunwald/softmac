e5c9c0e17e6a973c3c47a097e2c07232d6208a85
fifield <fifield@a9368e9a-fdf9-0310-b5ed-f0a418fb02d6>
2005-07-20 22:22:35 +0000

    nullmac update
    git-svn-id: https://esl.cs.colorado.edu/svn/softmac@146 a9368e9a-fdf9-0310-b5ed-f0a418fb02d6

--- trunk/nullmac/softmac_nullmac.c
+++ trunk/nullmac/softmac_nullmac.c
@@ -41,14 +41,22 @@
 MODULE_LICENSE("GPL");
 
 /* private instance data */
-typedef struct {
+struct nullmac_instance {
     CU_SOFTMAC_MACLAYER_INFO *macinfo;
     CU_SOFTMAC_PHYLAYER_INFO *phyinfo;
 
+    CU_SOFTMAC_MAC_RX_FUNC netif_rx;
+    void *netif_rx_priv;
+
+    struct tasklet_struct rxq_tasklet;
+    struct sk_buff_head   rxq;
+
     int id;
-} NULLMAC_INSTANCE;
+    rwlock_t lock;
+};
 
 static int nullmac_next_id;
+static const char *nullmac_name = "nullmac";
 
 /**
  * Every SoftMAC MAC or PHY layer provides a CU_SOFTMAC_LAYER_INFO interface
@@ -59,41 +67,123 @@ static CU_SOFTMAC_LAYER_INFO the_nullmac;
 static int
 nullmac_mac_attach(void *me, CU_SOFTMAC_PHYLAYER_INFO *phyinfo)
 {
-    NULLMAC_INSTANCE *inst = me;
+    struct nullmac_instance *inst = me;
+    int ret = -1;
+    unsigned long flags;
+
+    write_lock_irqsave(&(inst->lock), flags);
     if (!inst->phyinfo) {
 	inst->phyinfo = cu_softmac_phyinfo_get(phyinfo);
-	return 0;
+	ret = 0;
     }
-    /* error: already attached */
-    return -1;
+    write_unlock_irqrestore(&(inst->lock), flags);
+    return ret;
 }
 
 /* detach from phy layer */
 static int
 nullmac_mac_detach(void *me)
 {
-    NULLMAC_INSTANCE *inst = me;
+    //printk("%s\n", __func__);
+    struct nullmac_instance *inst = me;
+    unsigned long flags;
 
+    write_lock_irqsave(&(inst->lock), flags);
     if (inst->phyinfo) {
 	cu_softmac_phyinfo_free(inst->phyinfo);
 	inst->phyinfo = 0;
     }
+    write_unlock_irqrestore(&(inst->lock), flags);
     return 0;
 }
+
+static int
+nullmac_mac_set_netif_rx_func(void *me, 
+				CU_SOFTMAC_MAC_RX_FUNC rxfunc, 
+				void *rxpriv) 
+{
+    //printk("%s\n", __func__);
+    struct nullmac_instance *inst = me;
+    unsigned long flags;
+
+    write_lock_irqsave(&(inst->lock), flags);
+
+    inst->netif_rx = rxfunc;
+    inst->netif_rx_priv = rxpriv;
+
+    write_unlock_irqrestore(&(inst->lock), flags);
+
+    return 0;
+}
+
+static int
+nullmac_mac_packet_tx(void *me, struct sk_buff *skb, int intop)
+{
+    //printk("%s\n", __func__);
+    struct nullmac_instance *inst = me; 
+
+    BUG_ON(intop);
+
+    read_lock(&(inst->lock));
+    if (inst->phyinfo) {
+	inst->phyinfo->cu_softmac_phy_sendpacket(inst->phyinfo->phy_private, 1, skb);
+    }
+    read_unlock(&(inst->lock));
+
+    return CU_SOFTMAC_MAC_NOTIFY_OK;
+}
+
+static void
+nullmac_rx_tasklet(unsigned long data)
+{
+    //printk("%s\n", __func__);
+    struct nullmac_instance *inst = (struct nullmac_instance *)data;
+    struct sk_buff *skb;
+    
+    read_lock(&(inst->lock));
+
+    while ( (skb = skb_dequeue(&(inst->rxq))) ) {
+	if (inst->netif_rx)
+	    inst->netif_rx(inst->netif_rx_priv, skb);
+	else if (inst->phyinfo)
+	    inst->phyinfo->cu_softmac_phy_free_skb(inst->phyinfo->phy_private, skb);
+	else
+	    kfree_skb(skb);
+   }
+
+    read_unlock(&(inst->lock));
+}
+
+static int
+nullmac_mac_packet_rx(void *me, struct sk_buff *skb, int intop)
+{
+    //printk("%s\n", __func__);
+
+    struct nullmac_instance *inst = me;
+
+    skb_queue_tail(&(inst->rxq), skb);
+    if (intop) {
+	tasklet_schedule(&(inst->rxq_tasklet));
+    } else {
+	nullmac_rx_tasklet((unsigned long)me);
+    }
+    return CU_SOFTMAC_MAC_NOTIFY_OK;
+} 
 
 /* create and return a new nullmac instance */
 static void *
 nullmac_new_instance (void *layer_priv)
 {
-    printk("%s\n", __func__);
+    //printk("%s\n", __func__);
 
-    NULLMAC_INSTANCE *inst;
+    struct nullmac_instance *inst;
     void *ret = 0;
 
-    inst = kmalloc(sizeof(NULLMAC_INSTANCE), GFP_ATOMIC);
+    inst = kmalloc(sizeof(struct nullmac_instance), GFP_ATOMIC);
     if (inst) {
-	memset(inst, 0, sizeof(NULLMAC_INSTANCE));
+	memset(inst, 0, sizeof(struct nullmac_instance));
 
+	inst->lock = RW_LOCK_UNLOCKED;
 	inst->id = nullmac_next_id++;
 
 	/* setup the macinfo structure */
@@ -104,9 +194,16 @@ nullmac_new_instance (void *layer_priv)
 
 	/* override some macinfo functions */
 	/* the rest remain pointed at the default "do nothing" functions */
+	inst->macinfo->cu_softmac_mac_packet_tx = nullmac_mac_packet_tx;
+	inst->macinfo->cu_softmac_mac_packet_rx = nullmac_mac_packet_rx;
 	inst->macinfo->cu_softmac_mac_attach = nullmac_mac_attach;
 	inst->macinfo->cu_softmac_mac_detach = nullmac_mac_detach;
+	inst->macinfo->cu_softmac_mac_set_rx_func = nullmac_mac_set_netif_rx_func;
 
+	/* setup rx tasklet */
+	tasklet_init(&(inst->rxq_tasklet), nullmac_rx_tasklet, (unsigned long)inst);
+	skb_queue_head_init(&(inst->rxq));
+	
 	/* register with softmac */
 	cu_softmac_macinfo_register(inst->macinfo);
 
@@ -127,10 +224,9 @@ nullmac_new_instance (void *layer_priv)
 static void
 nullmac_free_instance (void *layer_priv, void *info)
 {
-    printk("%s\n", __func__);
-
+    //printk("%s\n", __func__);
     CU_SOFTMAC_MACLAYER_INFO *macinfo = info;
-    NULLMAC_INSTANCE *inst = macinfo->mac_private;
+    struct nullmac_instance *inst = macinfo->mac_private;
     if (inst->phyinfo) {
 	cu_softmac_phyinfo_free(inst->phyinfo);
     }
@@ -140,10 +236,9 @@ nullmac_free_instance (void *layer_priv, void *info)
 static int __init 
 softmac_nullmac_init(void)
 {
-    printk("%s\n", __func__);
-
+    //printk("%s\n", __func__);
     /* register the nullmac layer with softmac */
-    strncpy(the_nullmac.name, "nullmac", CU_SOFTMAC_NAME_SIZE);
+    strncpy(the_nullmac.name, nullmac_name, CU_SOFTMAC_NAME_SIZE);
     the_nullmac.cu_softmac_layer_new_instance = nullmac_new_instance;
     the_nullmac.cu_softmac_layer_free_instance = nullmac_free_instance;
     cu_softmac_layer_register(&the_nullmac);
@@ -154,8 +249,7 @@ softmac_nullmac_init(void)
 static void __exit 
 softmac_nullmac_exit(void)
 {
-    printk("%s\n", __func__);
-
+    //printk("%s\n", __func__);
     /* tell softmac we're leaving */
     cu_softmac_layer_unregister((CU_SOFTMAC_LAYER_INFO *)&the_nullmac);
 }
